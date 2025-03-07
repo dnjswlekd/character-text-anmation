502f036e475b3a8db6444671b249157a67be8acc
dnjswlekd <wj8014@gmail.com>
2025-03-07 12:45:15 +0900

    😛 제작 완료

--- scrpt.js
+++ scrpt.js
@@ -16,3 +16,17 @@ gsap.to('.stage', {
     scrub: 1,
   },
 });
+
+// DOM에서 클래스 이름이 "container"인 요소를 찾아 변수에 할당.
+const container = document.querySelector('.container');
+
+// 문서의 body 요소에 "mousemove" 이벤트가 발생할 때마다 실행되는 콜백 함수
+document.body.addEventListener('mousemove', (e) => {
+  const y = e.clientY - container.offsetTop;
+  // gsap 라이브러리의 to() 메서드를 사용해 container 요소의 y축 위치를 변경합니다.
+  gsap.to(container, { y: y - 50 }); //현재 y 마우스 좌표값
+
+  // gsap 라이브러리의 to() 메서드를 사용해 "mask" 클래스를 가진 요소의 y축 위치를 변경합니다.
+  // 여기서 변수y값만큼 빼 위치로 변경
+  gsap.to('.mask', { y: -y + 50 }); //y의 음수값
+});
